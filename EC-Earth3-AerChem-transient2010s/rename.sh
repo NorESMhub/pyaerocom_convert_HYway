9832c17509cb99540828ae17bdbdfd1be18ad889
Jan Griesfeller <jang@met.no>
2026-02-23 12:37:17 +0100

    initial git version

---
+++ EC-Earth3-AerChem-transient2010s/rename.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+#script to create data that is readable by pyaerocom
+
+IFS=$(echo -en "\n\b")
+
+usage() { echo "Usage: $0 [-U] <infiles>" 
+        echo "-U to update existing file with backup"
+        exit 0
+}
+
+if [[ $# -eq 0 ]]
+   then usage
+fi
+
+updateflag=0
+
+while getopts ":U" opt; do
+  case $opt in
+    U)
+      echo "-U was triggered!" >&2
+      arg=${OPTARG}
+      updateflag=1
+                #remove the -U from $@
+                shift 1
+      ;;
+    \?)
+      usage
+      ;;
+  esac
+done
+
+date=$(date '+%Y%m%d_%H%M%S')
+RND=${RANDOM}
+tmpfile="./dummy_${RND}.nc"
+cmd_nco="ncks -O -4 --chunk_policy nco"
+cmd_cdo="cdo -O -f nc4 -k auto copy"
+threedcmd="cdo -O -f nc4 -k auto copy"
+
+
+# get paths of some nco commands and cdo
+NCKS=$(mamba run -n nco which ncks)
+NCRENAME=$(mamba run -n nco which ncrename)
+NCATTED=$(mamba run -n nco which ncatted)
+NCWA=$(mamba run -n nco which ncwa)
+NCWA=$(mamba run -n nco which ncwa)
+CDO=$(mamba run -n cdo which cdo)
+
+set -x
+basedir='/nird/datapeak/NS11106K/HYway/modelling_repository/pyaerocom/'
+Model=`pwd | rev | cut -d/ -f1 | rev`
+outdir="${basedir}/${Model}/renamed/"
+lowest_level=0
+
+declare -A aerocom_vars
+aerocom_vars[o3]="vmro3"
+aerocom_vars[mmrso4]="mmrso4"
+aerocom_vars[ta]="ts"
+aerocom_vars[pfull]="ps"
+
+
+create_dir () {
+        dir=${1}
+        if [[ ! -d ${outdir} ]]
+                then 
+                mkdir -p ${outdir}
+                echo 'created'
+        else
+                echo 'exists'
+        fi
+}
+
+# for unit adjustments we need the temperature...
+# prepare yearly files of that...
+
+for file in "$@"
+	do echo ${file}
+	var=$(basename ${file} | cut -d_ -f1)
+	timecode=$(basename ${file} | cut -d_ -f2)
+	# split file into years
+	${CDO} -O splityear ${file} splityear_${RND}_
+	# extract lowest layer
+	for yearfile in $(find . -name "splityear_${RND}_*.nc" | sort)
+		do echo ${yearfile}
+		year=$(echo ${yearfile} | cut -d_ -f3 | cut -d. -f1)
+		#${CDO} -O splitlevel,0.992556 ${yearfile} splitlevel_${RND}_
+		${NCKS} -O -d lev,${lowest_level} -v ${var} ${yearfile} ${tmpfile}
+		${NCWA} -O -a lev ${tmpfile} ${tmpfile}
+		if [[ ${var} != ${aerocom_vars[${var}]} ]]
+			then ${NCRENAME} -O -v ${var},${aerocom_vars[${var}]} ${tmpfile}
+		fi
+		outfile="aerocom_${Model}_${aerocom_vars[${var}]}_Surface_${year}_${timecode}.nc"
+		# here some unit conversions will follow
+		if [[ ${var} == "o3" ]]
+			then mv ${tmpfile} renamed/${outfile}
+		elif [[ ${var} == "mmrso4" ]]
+			then mv ${tmpfile} renamed/${outfile}
+		elif [[ ${var} == "ta" ]]
+			then mv ${tmpfile} renamed/${outfile}
+		else
+			echo "ERROR: input variable ${var} not supported"
+			rm ${tmpfile}
+		fi
+		rm ${yearfile} 
+	done
+
+done
+
+
---
+++ ODDITIES.md
@@ -0,0 +1,68 @@
+# Oddities in the data files
+
+## EC-Earth3-AerChem
+- no mmrso4 data
+
+## GFDL-AM4.1
+- no ozone data?
+- Julian calendar
+
+## GFDL-ESM4-c1
+- Julian calendar; wrong variable name in file 
+
+According to the CMIP standard the variable name in the filename and the netcdf variable name are supposed to be the same (here: `o3` vs. `O3_dvmr`)
+```
+(base) [jang@login3-nird-lmd modelling_repository]$ ncdump -h /nird/datapeak/NS11106K/HYway/modelling_repository/GFDL-AM4.1/transient2010s/monthly_3d_native/O3_dvmr_transient2010s_GFDL-ESM4-c1_r1_gn_201001-201912.nc
+netcdf O3_dvmr_transient2010s_GFDL-ESM4-c1_r1_gn_201001-201912 {
+dimensions:
+	time = UNLIMITED ; // (120 currently)
+	pfull = 49 ;
+	lat = 180 ;
+	lon = 288 ;
+	bnds = 2 ;
+variables:
+	float O3_dvmr(time, pfull, lat, lon) ;
+		O3_dvmr:_FillValue = -1.e+10f ;
+		O3_dvmr:missing_value = -1.e+10f ;
+		O3_dvmr:units = "mol/mol" ;
+		O3_dvmr:long_name = "o3 (dry vmr)" ;
+		O3_dvmr:cell_methods = "time: mean" ;
+		O3_dvmr:interp_method = "conserve_order1" ;
+	double bnds(bnds) ;
+		bnds:long_name = "vertex number" ;
+	double lat(lat) ;
+		lat:long_name = "latitude" ;
+		lat:units = "degrees_N" ;
+		lat:axis = "Y" ;
+		lat:bounds = "lat_bnds" ;
+	double lat_bnds(lat, bnds) ;
+		lat_bnds:long_name = "latitude bounds" ;
+		lat_bnds:units = "degrees_N" ;
+		lat_bnds:axis = "Y" ;
+	double lon(lon) ;
+		lon:long_name = "longitude" ;
+		lon:units = "degrees_E" ;
+		lon:axis = "X" ;
+		lon:bounds = "lon_bnds" ;
+	double lon_bnds(lon, bnds) ;
+		lon_bnds:long_name = "longitude bounds" ;
+		lon_bnds:units = "degrees_E" ;
+		lon_bnds:axis = "X" ;
+	double pfull(pfull) ;
+		pfull:units = "mb" ;
+		pfull:long_name = "ref full pressure level" ;
+		pfull:axis = "Z" ;
+		pfull:positive = "down" ;
+	double time(time) ;
+		time:units = "days since 1870-01-01 00:00:00" ;
+		time:long_name = "time" ;
+		time:axis = "T" ;
+		time:calendar_type = "JULIAN" ;
+		time:calendar = "julian" ;
+		time:bounds = "time_bnds" ;
+	double time_bnds(time, bnds) ;
+		time_bnds:units = "days since 1870-01-01 00:00:00" ;
+		time_bnds:long_name = "time axis boundaries" ;
+
+```
+
